38c25a372adabfd76bf44b59441d5e4794efc6df
AndroidGxk <44991107+AndroidGxk@users.noreply.github.com>
2019-05-14 15:21:20 +0800

    首页

--- app/src/main/java/com/yikangcheng/admin/yikang/activity/fragment/Fragment_Shou.java
+++ app/src/main/java/com/yikangcheng/admin/yikang/activity/fragment/Fragment_Shou.java
@@ -9,17 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.yikangcheng.admin.yikang.R;
+import com.yikangcheng.admin.yikang.base.BaseFragment;
 
 /**
  * 作者：古祥坤 on 2019/5/14 11:59
  * 邮箱：1724959985@qq.com
  */
-public class Fragment_Shou extends Fragment {
-
-    @Nullable
+public class Fragment_Shou extends BaseFragment {
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shou, container, false);
-        return view;
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getFragmentLayoutId() {
+        return R.layout.fragment_shou;
     }
 }
